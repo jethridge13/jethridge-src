5b3711def5ee29d31302aa490260b159a3d466a8
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-09-16 22:26:48 -0400

    Minor: Very minor updates
    Removed most auto-generated starter code
    Added custom-header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomHeaderComponent } from './custom-header/custom-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomHeaderComponent
   ],
   imports: [
     BrowserModule
