2b6da6bba81a4ccbc870acafabcd3b19f7257d22
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-09-23 18:56:02 -0400

    Add headings and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CustomHeaderComponent } from './custom-header/custom-header.component';
 import { PictureComponent } from './picture/picture.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomHeaderComponent,
-    PictureComponent
+    PictureComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
