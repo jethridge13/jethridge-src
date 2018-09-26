4eaa6114d14d4890786993b082d67159abe8bed9
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-09-25 21:49:54 -0400

    Add more info to footer
    Added fontawesome dependencies and included GitHub and LinkedIn links
    to the footer, styling to come later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
 import { CustomHeaderComponent } from './custom-header/custom-header.component';
 import { PictureComponent } from './picture/picture.component';
@@ -14,7 +16,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+	faGithub = faGithub;
+	faLinkedin = faLinkedin;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
