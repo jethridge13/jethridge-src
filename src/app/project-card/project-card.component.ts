a6cccb5394b8b858f899b85e2dbf2a3930e19fb2
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-10-31 22:36:39 -0400

    Add slide animation for showing card details

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 import { Project } from '../project';
 import { SectionServiceService } from '../section-service.service';
@@ -7,7 +8,18 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 @Component({
 	selector: 'app-project-card',
 	templateUrl: './project-card.component.html',
-	styleUrls: ['./project-card.component.scss']
+	styleUrls: ['./project-card.component.scss'],
+	animations: [
+		trigger('slideInOut', [
+			transition('closed => open', [
+				style({transform: 'translateY(-100%)'}),
+				animate('200ms ease-in'), style({transform: 'translateY(0%)'})
+			]),
+			transition('open => closed', [
+				animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
+			])
+		])
+	]
 })
 export class ProjectCardComponent implements OnInit {
 
@@ -15,6 +27,7 @@ export class ProjectCardComponent implements OnInit {
 	viewMoreText = 'View Details';
 	faGithub = faGithub;
 	detailsVisible = false;
+	detailsState = 'closed';
 
 	constructor(private sectionService: SectionServiceService) { }
 
@@ -23,8 +36,26 @@ export class ProjectCardComponent implements OnInit {
 	}
 
 	toggleDetails(): void {
-		this.detailsVisible = !this.detailsVisible;
-		this.detailsVisible ? this.viewMoreText = 'Hide Details' : this.viewMoreText = 'View Details';
+		if (this.detailsState === 'closed') {
+			this.detailsState = 'open';
+		} else {
+			this.detailsState = 'closed';
+		}
+	}
+
+	animationStart(event: any): void {
+		console.log(this.detailsState);
+		if (event.toState === 'open') {
+			this.detailsVisible = true;
+			this.viewMoreText = 'Hide Details';
+		}
+	}
+
+	animationDone(event: any): void {
+		if (event.toState === 'closed') {
+			this.detailsVisible = false;
+			this.viewMoreText = 'View Details';
+		}
 	}
 
 }
