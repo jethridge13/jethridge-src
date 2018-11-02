588920e71277b90f285c4eef940f8b6f5011256b
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-11-01 20:41:35 -0400

    Add GWG details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,16 @@ export class AppComponent {
 	stonyBrookCard:Project = {title: 'Stony Brook University',
 		img:'./assets/img/SB.jpg',
 		subtitles:['Bachelors of Science in Computer Science'],
-		details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit sit amet nisl sed lacinia. Sed tempor, magna a suscipit molestie, nunc dolor lobortis arcu, vitae tempor magna purus ut tortor. Quisque sed nunc rutrum, viverra est ac, lobortis enim. Nullam feugiat augue mauris, ut venenatis arcu malesuada vel. Aenean hendrerit libero a turpis facilisis vestibulum. Aliquam magna mi, laoreet in odio eu, fringilla convallis lacus. Proin pellentesque, nunc bibendum bibendum semper, urna est ornare sapien, vitae pulvinar ante mi et dui.\n\nMorbi condimentum, velit non euismod mollis, nunc lacus scelerisque leo, eu elementum lacus nisi eget ipsum. Proin nisi velit, pellentesque tempus ultricies quis, interdum ac sem. Fusce gravida ligula non ante convallis pretium. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis facilisis iaculis bibendum. Praesent sed gravida arcu. Donec sodales quis tellus eu convallis. Aenean ultricies aliquet dui id iaculis. In rhoncus dolor neque, at imperdiet ligula sodales nec. Nunc turpis ex, viverra id dignissim a, consequat id nisl. Nullam efficitur magna sapien, id pretium massa aliquet vitae. In euismod ullamcorper urna vel consequat. In id tellus arcu. Donec augue augue, porttitor consectetur iaculis sit amet, maximus et elit.'
+		details: ''
 	};
 	growWithGoogleCard:Project = {title: 'Grow with Google Udacity Challenge',
 		img:'./assets/img/GWG.jpg',
 		subtitles:['Accepted Phase One: January 10, 2018',
-			'Accepted Phase Two: April 17, 2018']
+			'Accepted Phase Two: April 17, 2018'],
+		description: 'I was selected and inducted into the Udacity Grow With \
+		Google challenge based on my application in January, 2018. After my \
+		performance and community participation in phase one, I was chosen to \
+		move to stage two in April, 2018.'
 	};
 	MWSCard:Project = {title: 'Mobile Web Specialist Nanodegree',
 		img:'./assets/img/mws-nanodegree.svg',
