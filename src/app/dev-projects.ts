7a6104bdc77905a8687057abd52866386ea6b161
Joshua Ethridge <joshua.ethridge13@gmail.com>
2018-11-04 15:44:30 -0500

    Add Jethridge.com to projects

--- src/app/dev-projects.ts
+++ src/app/dev-projects.ts
@@ -15,6 +15,14 @@ export const PROJECTS: Project[] = [
 		github: 'https://github.com/jethridge13/mws-restaurant'
 	},
 	{
+		title: 'Jethridge.com',
+		description: 'That\'s right, I made this too! This site was actually \
+		my way of teaching myself Angular. After completing the Angular tutorial, \
+		I wanted to build something to help reinforce my understanding, so I made \
+		this!',
+		github: 'https://github.com/jethridge13/jethridge-src'
+	},
+	{
 		title: 'MWS Study Guide',
 		description: 'I made this study guide when I was studying for Google\'s \
 		Mobile Web Specialist Certification exam. It contains all of the links \
